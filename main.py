75084f164558404070a095be11739a1dc99747ed
Davi Torres <torresdavi04@gmail.com>
2020-12-05 15:11:39 -0300

    Adicionando arquivos que precisam ser lidos

---
+++ main.py
@@ -0,0 +1,3 @@
+## <tempo de inicialização>, <prioridade>, <tempo de processador>, <blocos em memória>,
+## <númerocódigo da impressora requisitada>, <requisição do scanner>, <requisição do modem>,
+## <númerocódigo do disco>
