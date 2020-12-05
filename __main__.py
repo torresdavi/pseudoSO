46d2d1fb935f1b954ebb4fc2d6261121debca72a
Davi Torres <torresdavi04@gmail.com>
2020-12-05 17:07:38 -0300

    Inserindo estrutura de pastas

--- __main__.py
+++ __main__.py
@@ -1,3 +1,8 @@
 ## <tempo de inicialização>, <prioridade>, <tempo de processador>, <blocos em memória>,
 ## <númerocódigo da impressora requisitada>, <requisição do scanner>, <requisição do modem>,
 ## <númerocódigo do disco>
+
+
+
+
+# operação 0 insere, operação 1 deleta
